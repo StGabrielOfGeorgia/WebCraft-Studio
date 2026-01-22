dbd68568845fca1ad6f373425361b1daf74ffe8b
StGabrielOfGeorgia <gabriel.corsairtnr@gmail.com>
2026-01-22 20:52:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
