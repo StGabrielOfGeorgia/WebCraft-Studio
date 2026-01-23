576460e95ac5da4d7a16893db9aebad007dab70c
StGabrielOfGeorgia <gabriel.corsairtnr@gmail.com>
2026-01-23 23:22:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite'
 
-export : defineConfig({
+export default defineConfig({
   base: './',
 
 export default defineConfig(({ mode }) => {
